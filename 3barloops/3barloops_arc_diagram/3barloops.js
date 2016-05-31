a30effbf31cf393ce01a632fbbebf6f8114d9c06
iyneedham <iyneedham@gmail.com>
2016-05-31 10:10:40 -0400

    Added a bunch of sankey charts

---
+++ 3barloops/3barloops_arc_diagram/3barloops.js
@@ -0,0 +1,43 @@
+// This file contains 3 bar loops from hip hop producers.
+
+var threebarloops = {
+  nodes:[
+    {nodeName:"Madlib", group:3},
+    {nodeName:"Prince Paul", group:3},
+    {nodeName:"Carl Mo", group:3},
+    {nodeName:"L.E.S.", group:3},
+    {nodeName:"Alchemist", group:3},
+    {nodeName:"Supa Dave West", group:3},
+    {nodeName:"J Dilla", group:3},
+    {nodeName:"Blu/MED/Madlib", group:2},
+    {nodeName:"Breeze Brewin", group:2},
+    {nodeName:"Big Boi/Bun B/Big Gee", group:2},
+    {nodeName:"Nas", group:2},
+    {nodeName:"Dilated Peoples", group:2},
+    {nodeName:"De La Soul", group:2},
+    {nodeName:"Busta Rhymes", group:2},
+    {nodeName:"Lucy Pearl", group:2},
+    {nodeName:"Mobb Deep", group:2},
+    {nodeName:"9th Wonder", group:1},
+    {nodeName:"A Tribe Called Quest", group:1},
+    {nodeName:"Beatnuts", group:1},
+    {nodeName:"Q Tip", group:1}
+  ],
+  links:[
+    {source:0, target:7, value:5},
+    {source:1, target:8, value:5},
+    {source:2, target:9, value:5},
+    {source:3, target:10, value:5},
+    {source:4, target:11, value:5},
+    {source:5, target:12, value:5},
+    {source:6, target:12, value:10},
+    {source:6, target:13, value:5},
+    {source:6, target:14, value:5},
+    {source:19, target:13, value:5},
+    {source:19, target:15, value:5},
+    {source:19, target:19, value:25},
+    {source:18, target:18, value:10},
+    {source:17, target:17, value:40},
+    {source:16, target:16, value:10}
+  ]
+};
